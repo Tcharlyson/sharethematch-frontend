918d2c9d57c59ea50f117587bf7bbdb2892ded9d
Macdows <malo.camaret@gmail.com>
2017-12-20 14:17:45 +0100

    Fix Navbar links

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,29 @@ import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-
+  makeNavbar = () => {
     const { isAuthenticated } = this.props;
+
+    if (isAuthenticated) {
+      return (
+        <Navbar.Collapse>
+          <Navbar.Text pullRight>Hi user</Navbar.Text>
+        </Navbar.Collapse>
+      )
+    } else {
+      return (
+        <Navbar.Collapse>
+          <Navbar.Text pullRight>
+            <Link to={`/signup`}>Register</Link>
+          </Navbar.Text>
+          <Navbar.Text pullRight>
+            <Link to={`/login`}>Login</Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      )
+    }
+  }
+  render() {
 
     return (
       <div className="App">
@@ -16,11 +36,7 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
-          <Navbar.Collapse>
-            <Navbar.Text pullRight>
-              {isAuthenticated ? 'Hi user' : <Link to={`/login`}>Login</Link> + '|' + <Link to={`/signup`}>Register</Link>}
-            </Navbar.Text>
-          </Navbar.Collapse>
+          {this.makeNavbar()}
         </Navbar>
       </div>
     );
